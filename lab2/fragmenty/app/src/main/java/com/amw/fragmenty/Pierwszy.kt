3bbb8476079baa6eaaa9fae07b522d8f07a1f09f
KrzysztofSuda30 <krzysztofsuda6@gmail.com>
2024-06-13 11:42:09 +0200

    '.'

---
+++ lab2/fragmenty/app/src/main/java/com/amw/fragmenty/Pierwszy.kt
@@ -0,0 +1,23 @@
+package com.amw.fragmenty
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.amw.fragmenty.databinding.FragmentPierwszyBinding
+class Pierwszy : Fragment() {
+    private var _binding: FragmentPierwszyBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentPierwszyBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
